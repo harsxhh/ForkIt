79efccc880845b3086723f438a17b5c214da1f69
harsxhh <harshydv0040@gmail.com>
2024-03-09 12:58:08 +0530

    Changes

--- pages/personalized-result.js
+++ pages/personalized-result.js
@@ -1,31 +1,113 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../src/layout/Layout';
-import Link from 'next/link';
+import axios from 'axios';
 
 const PersonalizedResult = () => {
     const [ageGroup, setAgeGroup] = useState('');
     const [jobType, setJobType] = useState('');
     const [physicalActivity, setPhysicalActivity] = useState('');
-
-    const handleAgeGroupChange = (value) => {
-        setAgeGroup(value);
+    const [recipes, setRecipes] = useState([]);
+     // Define the recipes state variable
+    const [recipes1, setRecipes1] = useState([]);   
+    const [recipes2, setRecipes2] = useState([]);
+   console.log(recipes);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.post('https://apis-new.foodoscope.com/recipe-search/recipesByNutrition', {
+                    energyMin: "500"
+                }, {
+                    headers: {
+                        Authorization: "Bearer 5gtCRn6CMFkrTs6p2RSyfUcuD_-lSfTznLlnxSxdSZgsDnZk",
+                        Accept: "application/json"
+                    }
+                });
+                const data = response.data;
+                console.log(data);
+                setRecipes(data.payload.data); // Set the recipes state here
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        if (ageGroup) { // Only fetch data if ageGroup state is truthy (i.e., not empty)
+            fetchData();
+        }
+    }, [ageGroup]); // Dependency array includes ageGroup state
+     // Empty dependency array ensures the effect runs only once
+     useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.post('https://apis-new.foodoscope.com/recipe-search/recipesByNutrition', {
+                    proteinMin: "80"
+                }, {
+                    headers: {
+                        Authorization: "Bearer 5gtCRn6CMFkrTs6p2RSyfUcuD_-lSfTznLlnxSxdSZgsDnZk",
+                        Accept: "application/json"
+                    }
+                });
+                const data = response.data;
+                console.log(data);
+                setRecipes1(data.payload.data); // Set the recipes state here
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        if (jobType) { // Only fetch data if ageGroup state is truthy (i.e., not empty)
+            fetchData();
+        }
+    }, [jobType]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.post('https://apis-new.foodoscope.com/recipe-search/recipesByNutrition', {
+                    proteinMin: "80"
+                }, {
+                    headers: {
+                        Authorization: "Bearer 5gtCRn6CMFkrTs6p2RSyfUcuD_-lSfTznLlnxSxdSZgsDnZk",
+                        Accept: "application/json"
+                    }
+                });
+                const data = response.data;
+                console.log(data);
+                setRecipes2(data.payload.data); // Set the recipes state here
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        if (physicalActivity) { // Only fetch data if ageGroup state is truthy (i.e., not empty)
+            fetchData();
+        }
+    }, [physicalActivity]);
+    const handleJobTypeChange = () => {
+        if(!jobType)
+        setJobType(true);
+        else
+        setJobType(false);
     };
 
-    const handleJobTypeChange = (value) => {
-        setJobType(value);
+    const handlePhysicalActivityChange = () => {
+        if(!physicalActivity)
+        setPhysicalActivity(true);
+        else
+        setPhysicalActivity(false); 
     };
 
-    const handlePhysicalActivityChange = (value) => {
-        setPhysicalActivity(value);
+    const handleAge = () => {
+        if(!ageGroup)
+        setAgeGroup(true);
+        else
+        setAgeGroup(false);
     };
-
     return (
         <Layout>
             <h2 className='pt-40 pb-40' style={{ textAlign: "center" }}>Get Personalized Recipes</h2>
             <section className="feature-section pb-250 rpb-70 rel z-1">
                 <div className="container">
                     <div className="row">
-                        <div className="col-xl-4 col-md-6">
+                        <div className="col-xl-4 col-md-6 " onClick={handleAge}>
                             <div className="feature-item wow fadeInUp delay-0-2s">
                                 <div className="content">
                                     <span className="number">01</span>
@@ -36,20 +118,10 @@ const PersonalizedResult = () => {
                                         />
                                     </div>
                                     <h4>Age Group</h4>
-                                    <div className="dropdown">
-                                        <select onChange={(e) => handleAgeGroupChange(e.target.value)}>
-                                            <option value="">Select Age Group</option>
-                                            <option value="Under 18">Under 18</option>
-                                            <option value="18-30">18-30</option>
-                                            <option value="30-50">30-50</option>
-                                            <option value="50-70">50-70</option>
-                                            <option value="70+">70+</option>
-                                        </select>
-                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-4 col-md-6">
+                        <div className="col-xl-4 col-md-6" onClick={handleJobTypeChange}>
                             <div className="feature-item color-two wow fadeInUp delay-0-4s">
                                 <div className="content">
                                     <span className="number">02</span>
@@ -61,48 +133,92 @@ const PersonalizedResult = () => {
                                         />
                                     </div>
                                     <h4>What type of Job?</h4>
-                                    <p>{jobType}</p>
-                                    <div className="dropdown">
-                                        <select onChange={(e) => handleJobTypeChange(e.target.value)}>
-                                            <option value="">Select Job Type</option>
-                                            <option value="Office Job">Office Job</option>
-                                            <option value="Manual Labor">Manual Labor</option>
-                                            <option value="Other">Other</option>
-                                        </select>
-                                    </div>
+                                    {/* Add your dropdown component here */}
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-4 col-md-6">
+                        <div className="col-xl-4 col-md-6" onClick={handlePhysicalActivityChange}>
                             <div className="feature-item color-three wow fadeInUp delay-0-6s">
                                 <div className="content">
                                     <span className="number">03</span>
                                     <div className="image">
                                         <img
-                                            src="assets/images/features/feature3.png"
+                                            src="assets/images/services/physical.png"
                                             alt="Feature"
+                                            style={{ width: "250px" }}
                                         />
                                     </div>
                                     <h4>Physical Activity</h4>
-                                    <p>{physicalActivity}</p>
-                                    <div className="dropdown">
-                                        <select onChange={(e) => handlePhysicalActivityChange(e.target.value)}>
-                                            <option value="">Select Physical Activity</option>
-                                            <option value="Sedentary">Sedentary</option>
-                                            <option value="Lightly Active">Lightly Active</option>
-                                            <option value="Moderately Active">Moderately Active</option>
-                                            <option value="Very Active">Very Active</option>
-                                            <option value="Extra Active">Extra Active</option>
-                                        </select>
-                                    </div>
+                                    {/* Add your dropdown component here */}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            {ageGroup && <section className="recipes-section">
+                <div className="container">
+                    <div className="row">
+                        {recipes.map((recipe, index) => (
+                            <div key={index} className="col-lg-4 col-md-6">
+                                <div className="recipe-card">
+                                    <img src={recipe.image} alt={recipe.title} />
+                                    <div className="recipe-details">
+                                        <h3>{recipe.title}</h3>
+                                        <p>Calories: {recipe?.Calories}</p>
+                                        <p>Energy: {recipe['Energy (kcal)']}</p>
+                                        <p>Cooking Time: {recipe?.cook_time}</p>
+                                        {/* Add other recipe details as needed */}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>}
+            {jobType && <section className="recipes-section">
+                <div className="container">
+                    <div className="row">
+                        {recipes1.map((recipe, index) => (
+                            <div key={index} className="col-lg-4 col-md-6">
+                                <div className="recipe-card">
+                                    <img src={recipe?.image} alt={recipe?.title} />
+                                    <div className="recipe-details">
+                                        <h3>{recipe?.title}</h3>
+                                        <p>Calories: {recipe?.Calories}</p>
+                                        <p>Energy: {recipe['Energy (kcal)']}</p>
+                                        <p>Cooking Time: {recipe?.cook_time}</p>
+                                        {/* Add other recipe details as needed */}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>}
+            {physicalActivity && <section className="recipes-section">
+                <div className="container">
+                    <div className="row">
+                        {recipes2.map((recipe, index) => (
+                            <div key={index} className="col-lg-4 col-md-6">
+                                <div className="recipe-card">
+                                    <img src={recipe?.image} alt={recipe?.title} />
+                                    <div className="recipe-details">
+                                        <h3>{recipe.title}</h3>
+                                        <p>Calories: {recipe?.Calories}</p>
+                                        <p>Energy: {recipe['Energy (kcal)']}</p>
+                                        <p>Cooking Time: {recipe?.cook_time}</p>
+                                        {/* Add other recipe details as needed */}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>}
         </Layout>
     );
 };
 
 export default PersonalizedResult;
+
